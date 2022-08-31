40496713547f938c93d8ef48336257d5e8c35e7c
Andre Dragacevac <a.dragacevac@icloud.com>
2022-08-31 09:19:58 -0400

    commit tree c6edb97b7fc65d22ad68f975a5066ec8d7a0d3a3

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Write your solution in this file!
+
+const employee = {};
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+  
+function updateEmployeeWithKeyAndValue(employee, streetAddress, value){
+return {
+    ...employee,
+    [streetAddress]:'11 Broadway',
+};
+}
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee,streetAddress,value) {
+    employee.streetAddress= '12 Broadway';
+    return employee;
+
+  }
+
+  beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+function deleteFromEmployeeByKey(employee,name){
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function destructivelyDeleteFromEmployeeByKey(employee,name  ){
+    delete employee.name;
+    return employee;
+  }
